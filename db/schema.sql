a063259a650c374f28f68bf03379da2016c4edfa
unknown <tetiana.lisina@gmail.comgit config --global user.name T8886>
2022-10-02 15:52:59 -0500

    revised schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,17 +4,35 @@ CREATE DATABASE registrar_db;
 USE registrar_db;
 
 CREATE TABLE departments (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  dapartment_name VARCHAR(30),
-  PRIMARY KEY (id)
+  department_id INT NOT NULL AUTO_INCREMENT,
+  dapartment VARCHAR(30),
+  PRIMARY KEY (department)
 );
 
 CREATE TABLE roles (
-  job_title VARCHAR(30) NOT NULL,
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  department VARCHAR(30) NOT NULL,
+  job_title VARCHAR(30) NOT NULL PRIMARY KEY,
+  role_id INT NOT NULL AUTO_INCREMENT,
+  department VARCHAR(30),
+  salary INT PRIMARY KEY,
+  FOREIGN KEY (department)
+  REFERENCES departments(department)
+  ON DELETE SET NULL
+);
+
+CREATE TABLE employees (
+  employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  job_title VARCHAR(30),
+  department VARCHAR(30),
   salary INT NOT NULL,
-  FOREIGN KEY (department_name)
-  REFERENCES departments(id)
+  manager VARCHAR(30),
+  FOREIGN KEY (department)
+  REFERENCES departments(department)
+  FOREIGN KEY (roles)
+  REFERENCES roles(job_title)
+  FOREIGN KEY (roles)
+  REFERENCES roles(salary)
+
   ON DELETE SET NULL
 );
